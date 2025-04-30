eaa988b3ddd8a3155da762b36a52ff5606817b13
tmetrue <torstein.meland@gmail.com>
2025-04-30 13:48:30 +0200

    Use backend at port 8888

--- frontend/src/services/taskService.ts
+++ frontend/src/services/taskService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Task } from '../types/Task';
 
-const API_URL = 'http://localhost:8080/api/tasks';
+const API_URL = 'http://localhost:8888/api/tasks';
 
 export const taskService = {
     async getAllTasks(): Promise<Task[]> {
